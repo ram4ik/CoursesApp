50cf5f0b05b3e46ff72fd11e1efe58753e343e40
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-06-26 13:22:59 +0300

    Initial Commit

---
+++ CoursesApp/CoursesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesAppApp.swift
+//  CoursesApp
+//
+//  Created by Ramill Ibragimov on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
